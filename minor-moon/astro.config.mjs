c137baa88f659e395b3fcb24e0520f712caaa2b0
Pratham Kwatra <pk588@drexel.edu>
2026-02-14 00:50:55 -0500

    build: setup GitHub Actions for Astro deployment and remove Jekyll files

--- minor-moon/astro.config.mjs
+++ minor-moon/astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://prathamkwatra.github.io",
 	integrations: [tailwind()],
 });
